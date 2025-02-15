aa3a74d45c47955543c50c349e9a6b35eddbb814
TangoCh4rlie <elouanreymond@tutanota.com>
2025-02-15 21:15:21 +0100

    feat: camera movement init

--- src/components/CameraInit.tsx
+++ src/components/CameraInit.tsx
@@ -1,19 +1,42 @@
-import { useThree } from "@react-three/fiber";
-import { useEffect } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
 import * as THREE from "three";
 
 const SetInitialCameraPosition = () => {
   const { camera } = useThree();
+  const initialPosition = new THREE.Vector3(600, 600, 600);
+  const targetPosition = new THREE.Vector3(
+    60 * Math.sin(THREE.MathUtils.degToRad(15)),
+    60 * Math.sin(THREE.MathUtils.degToRad(15)),
+    60,
+  );
+  const progress = useRef(0);
+  const duration = 300; // Duration of the animation in frames
+
   useEffect(() => {
-    // Set camera position to achieve a 15-degree angle view
-    camera.position.set(
-      10 * Math.sin(THREE.MathUtils.degToRad(15)),
-      10 * Math.sin(THREE.MathUtils.degToRad(15)),
-      10,
-    );
+    camera.position.copy(initialPosition);
     camera.lookAt(0, 0, 0);
   }, [camera]);
+
+  useFrame(() => {
+    if (progress.current < 1) {
+      progress.current += 1 / duration;
+      const easedProgress = easeInOutQuad(progress.current);
+      camera.position.lerpVectors(
+        initialPosition,
+        targetPosition,
+        easedProgress,
+      );
+      camera.lookAt(0, 0, 0);
+    }
+  });
+
   return null;
+};
+
+// Easing function for smooth acceleration and deceleration
+const easeInOutQuad = (t: number) => {
+  return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
 };
 
 export default SetInitialCameraPosition;
