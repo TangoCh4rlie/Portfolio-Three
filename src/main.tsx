ba95cb26aa494b2a5d39af697655abbc1037f29e
TangoCh4rlie <elouanreymond@tutanota.com>
2025-02-16 21:19:47 +0100

    feat: posthog

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,23 @@ import App from "./App.tsx";
 import TargetProvider from "./utils/TargetProvider.tsx";
 import ModalProvider from "./utils/ModalProvider.tsx";
 
+import { PostHogProvider } from "posthog-js/react";
+
+const options = {
+  api_host: import.meta.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+};
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TargetProvider>
-      <ModalProvider>
-        <App />
-      </ModalProvider>
-    </TargetProvider>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <TargetProvider>
+        <ModalProvider>
+          <App />
+        </ModalProvider>
+      </TargetProvider>
+    </PostHogProvider>
   </StrictMode>,
 );
